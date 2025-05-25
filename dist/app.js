1ac4935c11b7d1d538c382f92af6c58192e90a07
xOmilk <leite46052@gmail.com>
2025-05-25 14:46:12 -0300

    Recuperando o objeto Movies e acessando atributos

--- dist/app.js
+++ dist/app.js
@@ -8,23 +8,74 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+//Função promete retornar uma resposta ou null
 function getMovies(id) {
     return __awaiter(this, void 0, void 0, function* () {
-        const url = `https://api.potterdb.com/v1/movies/1`;
-        let result;
+        const url = id === undefined ? `https://api.potterdb.com/v1/movies` : `https://api.potterdb.com/v1/movies/${id}`;
+        id ? console.log("O id fornecido foi", id) : console.log("Procurando todos os filmes.");
         try {
+            let result;
             result = yield fetch(url);
             let data = yield result.json();
-            console.log(data.data);
-            return data.data;
+            if (data.data) {
+                //ver o filme retornado
+                //console.log(data.data);
+                return data.data;
+            }
         }
         catch (error) {
             console.log("Erro:", error.message);
         }
+        return null;
     });
 }
-function printMovies() {
+function getAllMovies() {
     return __awaiter(this, void 0, void 0, function* () {
+        const movie = yield getMovies();
+        if (movie === null) {
+            throw new Error("NO MOVIES WAS FOUND!");
+        }
+        console.log("Todos os filmes:", movie);
+        return movie;
     });
 }
-getMovies(1);
+function searchMovie(movieId) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const movie = yield getMovies(movieId);
+        if (movie === null) {
+            console.log("Não foi possivel encontrar o filme procurado");
+            return false;
+        }
+        else {
+            console.log("O filme procurado foi: ", movie);
+            return movie;
+        }
+    });
+}
+function setMovie(movieId) {
+    return __awaiter(this, void 0, void 0, function* () {
+        var _a;
+        const movie = yield searchMovie(movieId);
+        if (typeof movie === 'boolean') {
+            console.log('Não foi possivel encontrar');
+        }
+        else {
+            console.log(movie.attributes.poster);
+            let divImage = document.createElement("div");
+            divImage.classList.add("img");
+            let posterImg = document.createElement("img");
+            posterImg.src = movie.attributes.poster;
+            divImage.appendChild(posterImg);
+            (_a = document.getElementById("main")) === null || _a === void 0 ? void 0 : _a.appendChild(divImage);
+        }
+    });
+}
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        let id = "94055b36-c4dd-4ae5-aede-dd6b6e67e107";
+        yield getAllMovies();
+        console.log("------------------------");
+        yield setMovie(id);
+    });
+}
+main();
--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,101 @@
+type Movies = {
+    attributes: {
+        title: string,
+        poster: string,
+        release_date: string,
+        running_time: string,
+        trailer: string,
+
+    }
+}
+
+//Função promete retornar uma resposta ou null
+async function getMovies(id?: string): Promise<Response | null> {
+
+    const url = id === undefined ? `https://api.potterdb.com/v1/movies` : `https://api.potterdb.com/v1/movies/${id}`
+    id! ? console.log("O id fornecido foi", id) : console.log("Procurando todos os filmes.");
 
 
-async function getMovies(id: number) {
-    const url = `https://api.potterdb.com/v1/movies/1`
-
-    let result
     try {
+        let result
 
         result = await fetch(url)
         let data = await result.json()
 
-        console.log(data.data);
-
-        return data.data;
+        if (data.data!) {
+            //ver o filme retornado
+            //console.log(data.data);
+            return data.data;
+        }
     }
     catch (error: any) {
-
         console.log("Erro:", error.message);
-
-
     }
 
+    return null;
+}
+
+async function getAllMovies(): Promise<Response> {
+    const movie = await getMovies()
+    if (movie === null) {
+        throw new Error("NO MOVIES WAS FOUND!");
+    }
+    console.log("Todos os filmes:", movie);
+    return movie;
+}
+
+async function searchMovie(movieId: string): Promise<Response | false> {
+
+    const movie = await getMovies(movieId)
+    if (movie === null) {
+        console.log("Não foi possivel encontrar o filme procurado");
+        return false;
+    }
+    else {
+        console.log("O filme procurado foi: ", movie);
+        return movie;
+    }
 
 }
 
-async function printMovies() {
+async function setMovie(movieId: string) {
 
+    const movie = await searchMovie(movieId) as Movies | boolean;
 
+    if (typeof movie === 'boolean') {
+        console.log('Não foi possivel encontrar');
+
+    }
+    else {
+
+        console.log(movie.attributes.poster);
+
+        let divImage = document.createElement("div");
+        divImage.classList.add("img");
+        let posterImg = document.createElement("img");
+        posterImg.src = movie.attributes.poster;
+        divImage.appendChild(posterImg);
+        document.getElementById("main")?.appendChild(divImage);
+    }
 
 }
 
-getMovies(1)
+
+
+async function main() {
+
+    let id = "94055b36-c4dd-4ae5-aede-dd6b6e67e107";
+
+    await getAllMovies();
+    console.log("------------------------");
+
+    await setMovie(id);
+}
+main();
+
+
+
+
+
 
 
